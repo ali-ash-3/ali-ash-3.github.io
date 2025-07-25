5a6f84a519d06dfbcb10144e166e1d4950c86da9
Muhammad Ali Ashraf <134807254+ali-ash-3@users.noreply.github.com>
2025-07-25 15:54:05 +0500

    chore: added analytics code to script.js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+async src="https://www.googletagmanager.com/gtag/js?id=G-TXP18CV6FB">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TXP18CV6FB');
